b273c652c25ac6788e0b61b9887d58951285b3de
carlospolli <carlos.polli.cp1@votorantim.com>
2019-11-08 17:45:53 -0300

    Nova Pagina e começando a pagina de Carga

--- js/teste.js
+++ js/teste.js
@@ -5,3 +5,27 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+
+$('#dpRM').datetimepicker({
+    viewMode : 'months',
+    format : 'MM/YYYY',
+    toolbarPlacement: "top",
+    allowInputToggle: true,
+    icons: {
+        time: 'fa fa-time',
+        date: 'fa fa-calendar',
+        up: 'fa fa-chevron-up',
+        down: 'fa fa-chevron-down',
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+    }
+});
+
+$("#dpRM").on("dp.show", function(e) {
+   $(e.target).data("DateTimePicker").viewMode("months"); 
+});
