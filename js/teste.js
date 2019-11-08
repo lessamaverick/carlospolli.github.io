d674f7d695b980a1ec5af1cf024932fab7117426
carlospolli <carlos.polli.cp1@votorantim.com>
2019-11-08 09:26:24 -0300

    Colocando NavBar

--- js/teste.js
+++ js/teste.js
@@ -1,1 +1,7 @@
-alert("teste")
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
