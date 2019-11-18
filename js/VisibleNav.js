a625ac30b9cf02312be3cf89e5786083ff0d9ec8
carlospolli <carlos.polli.cp1@votorantim.com>
2019-11-18 13:54:03 -0300

    Adicionando tabelas editaveis, seletor de meses e anos e aviso ao enviar na páginas de carga.

--- js/VisibleNav.js
+++ js/VisibleNav.js
@@ -1,5 +1,5 @@
 
 window.onload = function myFunction() {
-var element = document.getElementById("NavBar");
-element.classList.add("shrink");
-};
+    var element = document.getElementById("NavBar");
+    element.classList.add("shrink");
+    };
--- js/monthyearpicker.js
+++ js/monthyearpicker.js
@@ -1,17 +1,27 @@
-$(this).datepicker({
-    changeMonth: true,
-    changeYear: true,
-    dateFormat: "M yy",
-    showButtonPanel: true,
-    currentText: "This Month",
-    onChangeMonthYear: function (year, month, inst) {
-      $(this).val($.datepicker.formatDate('M yy', new Date(year, month - 1, 1)));
-    },
-    onClose: function(dateText, inst) {
-      var month = $(".ui-datepicker-month :selected").val();
-      var year = $(".ui-datepicker-year :selected").val();
-      $(this).val($.datepicker.formatDate('M yy', new Date(year, month, 1)));
-    }
-  }).focus(function () {
-    $(".ui-datepicker-calendar").hide();
-  });
+var startDate = new Date();
+var fechaFin = new Date();
+var FromEndDate = new Date();
+var ToEndDate = new Date();
+
+$('.from').datepicker({
+    autoclose: true,
+    minViewMode: 1,
+    format: 'mm/yyyy'
+}).on('changeDate', function(selected){
+        startDate = new Date(selected.date.valueOf());
+        startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
+        $('.to').datepicker('setStartDate', startDate);
+    }); 
+
+$('.to').datepicker({
+    autoclose: true,
+    minViewMode: 1,
+    format: 'mm/yyyy'
+}).on('changeDate', function(selected){
+        FromEndDate = new Date(selected.date.valueOf());
+        FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
+        $('.from').datepicker('setEndDate', FromEndDate);
+    });
+
+
+
